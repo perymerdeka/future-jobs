e1d0dd49d14c94aab86ac8bffdf4ad6f0ff95220
perymerdeka <tkjasikk@gmail.com>
2021-10-05 05:46:21 +0700

    Done Adding Text to OnBoardingScreen

--- lib/pages/onboarding_screen.dart
+++ lib/pages/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
@@ -13,9 +14,31 @@ class OnBoardingScreen extends StatelessWidget {
             decoration: const BoxDecoration(
                 image: DecorationImage(
                     image: AssetImage('assets/icons/bg.png'),
-                    fit: BoxFit.cover
-                  )
+                    fit: BoxFit.cover)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50, right: 44, left: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Build Your Next\nFuture Career Like\na Master',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
+                const SizedBox(height: 20),
+                Text(
+                  '18,000 jobs available',
+                  style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300),
+                )
+              ],
+            ),
           )
         ],
       ),
