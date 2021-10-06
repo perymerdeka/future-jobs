fa2eb04730a1ab09468568ca2314c613a107dcf8
perymerdeka <tkjasikk@gmail.com>
2021-10-06 08:01:49 +0700

    Done Add TextFormField in Sign Up

--- lib/pages/sign_up_screen.dart
+++ lib/pages/sign_up_screen.dart
@@ -13,27 +13,140 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 20, left: 24, right: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Sign Up', style: titleTextStyle,),
-            Text('Begin New Journey', style: subtitleTextStyle,),
-             const SizedBox(
-                height: 40,
-              ),
-              Center(
-                child: Column(children: [
-                  Image.asset(
-                    'assets/icons/user_pic.png',
-                    width: 120,
-                    height: 120,
-                  )
-                ]),
-              ),
-          ],
-        ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 24, right: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Sign Up',
+                  style: titleTextStyle,
+                ),
+                Text(
+                  'Begin New Journey',
+                  style: subtitleTextStyle,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Center(
+                  child: Column(children: [
+                    Image.asset(
+                      'assets/icons/user_pic.png',
+                      width: 120,
+                      height: 120,
+                    )
+                  ]),
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Full Name',
+                      style: titleTextStyle,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          fillColor: const Color(0xffF1F0F5),
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(100))),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Email Address',
+                      style: titleTextStyle,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          fillColor: const Color(0xffF1F0F5),
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(100))),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Password',
+                      style: titleTextStyle,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          fillColor: const Color(0xffF1F0F5),
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(100))),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Your Goal',
+                      style: titleTextStyle,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          fillColor: const Color(0xffF1F0F5),
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(100))),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
