8866e0e7af5c4f91bb9c720ab58ea107efa1ee91
perymerdeka <tkjasikk@gmail.com>
2021-10-09 13:58:03 +0700

    Done Add Hot Category

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:future_jobs/theme/login_theme.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePageScreen extends StatelessWidget {
   static const routeName = '/home';
@@ -27,16 +28,61 @@ class HomePageScreen extends StatelessWidget {
                 ],
               ),
               const Spacer(),
-              Image.asset('assets/icons/user_pic.png', width: 58, height: 58,)
+              Image.asset(
+                'assets/icons/user_pic.png',
+                width: 58,
+                height: 58,
+              )
             ],
           ),
         ),
       );
     }
 
+    Widget body() {
+      return Padding(
+        padding: const EdgeInsets.only(left: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hot Categories',
+              style: GoogleFonts.poppins(
+                  color: const Color(0xff272C2F),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Container(
+              width: 150,
+              height: 200,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/card/card_category.png'),
+                  ),
+                ),
+              child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16, bottom: 16),
+                child: Text('Mobile Developer', style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 18,
+                ),),
+              ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       body: Column(
-        children: [headers()],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [headers(), body()],
       ),
     );
   }
